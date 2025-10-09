2d075e35d05cec10f835dccda9f0ab8d8c60c80b
felissedano <contact@felissedano.com>
2025-10-09 15:02:18 -0400

    chore: enable the use of dev mail server during local development

--- alternative_naissance/alternative_naissance/settings/dev.py
+++ alternative_naissance/alternative_naissance/settings/dev.py
@@ -9,7 +9,11 @@ SECRET_KEY = "django-insecure-0k(u8qb7urcpn$+65fb_&n7fpqbwvb%!pl7(q^m$_ww51kb)gz
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 21025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 try:
