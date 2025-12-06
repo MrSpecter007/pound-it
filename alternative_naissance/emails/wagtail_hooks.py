fb3fdfd175570a28cefba69e9865a6890a2307eb
felissedano <contact@felissedano.com>
2025-12-06 00:06:16 -0500

    chore: disable EmailTemplate deletion functionality

--- alternative_naissance/emails/wagtail_hooks.py
+++ alternative_naissance/emails/wagtail_hooks.py
@@ -1,4 +1,9 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from wagtail import hooks
 from wagtail.admin.panels import FieldPanel
+from wagtail.admin.widgets import ListingButton
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 
@@ -21,3 +26,21 @@ class EmailTemplateViewSet(SnippetViewSet):
     icon = 'mail'
     list_display = ('scenario', 'scenario_description')
     add_to_admin_menu = True
+
+
+@hooks.register('construct_snippet_listing_buttons')
+def remove_delete_button_in_email_template(buttons: list[ListingButton], snippet, user):
+    if issubclass(type(snippet), EmailTemplate):
+        for button in buttons:
+            if button.label == 'Delete':
+                buttons.remove(button)
+
+
+@hooks.register("before_bulk_action")
+def disallow_delete_email_template(request, action_type, objects: list,
+                                   action_class_instance) -> HttpResponseRedirect | None:
+    if action_type == 'delete' and len(objects) > 0 and issubclass(type(objects[0]), EmailTemplate):
+        messages.error(request, "Vous ne pouvez pas supprimer les Modèles du Courrier")
+        return redirect("/admin/snippets/emails/emailtemplate/")
+    else:
+        return None
