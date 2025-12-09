d566ee5096766ea6ae583a915fd3ca3d04d905df
felissedano <contact@felissedano.com>
2025-12-09 11:53:12 -0500

    fix: add missing comma

--- alternative_naissance/alternative_naissance/urls.py
+++ alternative_naissance/alternative_naissance/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
 
-    path("service-request/", include(servicerequests_urls))
+    path("service-request/", include(servicerequests_urls)),
 
     path("", include("core.urls")),
 
