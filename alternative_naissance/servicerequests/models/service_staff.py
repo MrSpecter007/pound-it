1975f46577372d7c3ca85015e923a85ea4e5a7ca
felissedano <contact@felissedano.com>
2026-01-13 13:34:32 -0500

    fix: resolve accessing ServiceStaff class before migration issue

--- alternative_naissance/servicerequests/models/service_staff.py
+++ alternative_naissance/servicerequests/models/service_staff.py
@@ -13,6 +13,16 @@ class ServiceStaff(models.Model):
     def __str__(self):
         return self.full_name
 
+    @staticmethod
+    def get_accompagnant_service_staff_names():
+        """Returns a list of full names of all service staff members that are accompagnant. Used in the ACCOMPAGNANT.E and INTERVENANT.E using DatalistInput widget."""
+        return list(ServiceStaff.objects.filter(is_accompagnant=True).values_list("full_name", flat=True))
+
+    @staticmethod
+    def get_intervenant_service_staff_names():
+        """Returns a list of full names of all service staff members that are intervenant. Used in the INTERVENANT.E using DatalistInput widget."""
+        return list(ServiceStaff.objects.filter(is_intervenant=True).values_list("full_name", flat=True))
+
     class Meta:
         verbose_name = "Accompagante.e / Intervenante.e"
         verbose_name_plural = "Accompagnantes.es / Intervenantes.es"
